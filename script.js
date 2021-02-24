5f04165a494ff72d3634f95f5f457d338a7f9153
shahirk48 <42793050+shahirk48@users.noreply.github.com>
2021-02-24 07:29:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('https://github.com/shahirk48/detectface/blob/main/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('https://github.com/shahirk48/detectface/blob/main/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('https://github.com/shahirk48/detectface/blob/main/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('https://github.com/shahirk48/detectface/blob/main/models'),
 ]).then(startVideo)
 
 function startVideo() {
